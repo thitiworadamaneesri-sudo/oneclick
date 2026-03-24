3c726b4e8db12d6322760f45d116dd8d500ad904
Thitiworada Maneesri <thitiworada@easternsugar.corp>
2026-03-24 11:33:07 +0700

    fix showrooms api path

--- src/main/java/com/oneclick/oneclickpro/controller/ShowroomsController.java
+++ src/main/java/com/oneclick/oneclickpro/controller/ShowroomsController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("/api/showrooms")
+@RequestMapping("/api")
 public class ShowroomsController {
 
     private final JdbcTemplate jdbcTemplate;
