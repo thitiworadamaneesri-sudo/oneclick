4a7317b29c27a23006814336a47c903bb7658e4c
Thitiworada Maneesri <thitiworada@easternsugar.corp>
2026-03-24 17:24:32 +0700

    fix health endpoint again

--- src/main/java/com/oneclick/oneclickpro/OneclickproApplication.java
+++ src/main/java/com/oneclick/oneclickpro/OneclickproApplication.java
@@ -2,12 +2,19 @@ package com.oneclick.oneclickpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OneclickproApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OneclickproApplication.class, args);
     }
 
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 }
