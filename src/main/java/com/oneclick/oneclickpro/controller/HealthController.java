ecc5594411f3af9cd98b5dea471dc0bbfe59ed36
Thitiworada Maneesri <thitiworada@easternsugar.corp>
2026-03-25 10:34:43 +0700

    add global cors config

--- src/main/java/com/oneclick/oneclickpro/controller/HealthController.java
+++ src/main/java/com/oneclick/oneclickpro/controller/HealthController.java
@@ -1,16 +1,21 @@
 package com.oneclick.oneclickpro.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {
+    "http://localhost:5173",
+    "https://ppavis.com"
+})
 @RestController
 @RequestMapping("/api")
 public class HealthController {
 
     public HealthController() {
-    System.out.println("🔥 HealthController LOADED");
-}
+        System.out.println("🔥 HealthController LOADED");
+    }
 
     @GetMapping("/health")
     public String health() {
