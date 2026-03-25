1b56b01e6eb3b141cac2b6f8deeb4a8a61296f53
Thitiworada Maneesri <thitiworada@easternsugar.corp>
2026-03-25 21:57:42 +0700

    fix cors config

--- src/main/java/com/oneclick/oneclickpro/config/CorsConfig.java
+++ src/main/java/com/oneclick/oneclickpro/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5174")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
